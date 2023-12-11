58d51b0b40e09477c72a11f31e4868d882afca16
Yakov Petrukhin <yakov.petrukhin@gmail.com>
2023-12-10 22:50:19 -0500

    Initial Commit

---
+++ Expense-Tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Expense-Tracker
+//
+//  Created by Yakov Petrukhin on 2023-12-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Expense-Tracker/Expense_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_TrackerApp.swift
+//  Expense-Tracker
+//
+//  Created by Yakov Petrukhin on 2023-12-10.
+//
+
+import SwiftUI
+
+@main
+struct Expense_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
